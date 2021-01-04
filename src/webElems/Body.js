af00f11929b92d1ced01a1cb0a40a47d7c8d15e8
Tommy-Wilkinson <tommy.wilkinson@gmail.com>
2021-01-03 16:05:28 -0800

    3 files modified, 1 added

--- src/webElems/Body.js
+++ src/webElems/Body.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Input from "./Input.js";
 
 export default class Body extends React.Component{
   render(){
     return(
       <>
         <p> This is where the body is.</p>
-        <p> Still a test </p>
+        <p> <Input /> </p>
         <p> Stuff also goes here</p>
 
     </>
