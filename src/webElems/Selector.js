dd6119ea709606710090135443d48dd23c6b7c1e
Tommy-Wilkinson <tommy.wilkinson@gmail.com>
2021-01-03 18:04:43 -0800

    1 file modified

--- src/webElems/Selector.js
+++ src/webElems/Selector.js
@@ -2,11 +2,22 @@ import React from "react";
 import options from "./SelOptions.js";
 
 export default class Selector extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      value: "Third",
+    }
+    this.handlechange = this.handlechange.bind(this);
+  }
 
+  handlechange(e){
+      console.log("Selection changed to " + e.target.value);
+      this.setState({value: e.target.value});
+  }
   render(){
     return(
       <>
-        <select value="Third">
+        <select value={this.state.value} onChange={this.handlechange}>
             {options.map((option) => (
               <option value={option.value}>{option.label}</option>
             ))}
