2015f11bff8a71583958bc2793a844ff6e9e4360
Tommy <Tommy.wilkinson@gmail.com>
2021-01-15 23:05:34 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 //import Extra from "./extras.js";
-import {Extra, Extra1, Extra2, Extra3, changeExtra1Name} from  "./extras.js";
-import * as extrastuff from "./extras.js";
+
 
 //export default function App() {
 //  return (
@@ -23,95 +22,6 @@ const user = {
 
 export default function App(){
 
-    return(<Game />);
+ return(nul);
 
 }
-
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({value:"X"})}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-const Extra1obj = new Extra1({newname: "Tommy"});
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">          
-          <Board />
-          <InfoTag />
-        </div>
-        <div className="game-info">    
-          <Extra />
-          <Extra1 newName={["Mr. ", user.last]}/>
-          <Extra2 />
-          <extrastuff.Extra3 />
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-
-class InfoTag extends React.Component
-{
-  render(){
-    return(
-      <p>
-      Game for {returnName(user)}
-      </p>
-    );
-  }
-}
-
-function returnName(user)
-{
-  return user.first + " " + user.last;
-}
-
